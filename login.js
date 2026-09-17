b234f1adb5cfcba9abb68fa0d138e7cce32d7420
Nguyễn Thành Anh Đức <nguyenthanhanhduclop9a2@gmail.com>
2026-09-17 18:50:38 +0700

    Add login function for user authentication
    Implement login function to validate user credentials.

---
+++ login.js
@@ -0,0 +1,14 @@
+/**
+ * Hàm kiểm tra đăng nhập
+ * @param {string} username - Tên đăng nhập
+ * @param {string} password - Mật khẩu
+ * @returns {boolean} - true nếu đúng thông tin đăng nhập, ngược lại false
+ */
+function login(username, password) {
+    return username === 'admin' && password === '123';
+}
+
+// Export để sử dụng trong Jest (CommonJS)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { login };
+}
